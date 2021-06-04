a045bb58dcffb3462401ea13a1c039e53d9b6526
paporotnuk1 <nikgawrik@yandex.ru>
2021-06-04 08:50:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// import packages
+const express = require('express');
+const mongoose = require('mongoose');
+const ansiColors = require('ansi-colors');
+const {join} = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// middleware
+const morgan = require('morgan');
+const favicon = require('serve-favicon');
+
+app.use(morgan('dev'));
+app.use(favicon(join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// routes
+app.use((req, res) => {
+    res.sendStatus(404);
+});
+
+// server startup
+const {PORT, ConnectionStrings} = require('./config/default.json');
+
+// mongoose.connect(ConnectionStrings.Users, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// }, (error) => {
+//     if(error) return console.log(error.message);
+
+    app.listen(
+        PORT,
+        console.log(
+            ansiColors.bgGreen.white(`server is listening on http://localhost:${PORT}`)
+        )
+    )
+// });
