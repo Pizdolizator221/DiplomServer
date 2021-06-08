24e4b38a0cfe1406e73f27cb308a6f750de511da
Pizdolizator221 <chungus@DESKTOP-K72F2S0.localdomain>
2021-06-08 20:02:22 +0800

    hz

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 // routes
 const userRouter = require('./routers/userRouter');
 app.use('/api/users', userRouter);
